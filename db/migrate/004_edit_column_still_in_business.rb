f0741fd10f514ed12a4125934ca42df5be558d8c
Brad Schrag <brad.schrag@gmail.com>
2019-01-09 14:42:46 +0000

    Automatically backed up by Learn

--- db/migrate/004_edit_column_still_in_business.rb
+++ db/migrate/004_edit_column_still_in_business.rb
@@ -1,4 +1,4 @@
-class EditColumnStillInBusiness < ActiveRecord::Migrate[5.1]
+class EditColumnStillInBusiness < ActiveRecord::Migration[5.1]
   def change
     change_column :costume_stores, :open_status, :still_in_business
   end
